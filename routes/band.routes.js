b86b84da390e681cc45cbed6fe48eb7f93f24898
Ronnie Vélez <ronnievelz@gmail.com>
2024-06-01 00:12:23 +0200

    random band call done

--- routes/band.routes.js
+++ routes/band.routes.js
@@ -10,6 +10,7 @@ router.post("/", (req, res, next) => {
     genre: req.body.genre,
     country: req.body.country,
     crew:req.body.crew
+    //owner:-----comes from...authtoken friday
   })
     .then(() => {
       console.log("band created");
@@ -21,9 +22,11 @@ router.post("/", (req, res, next) => {
 });
 //localhost:5005/api/band---------list of the bands---------------
 router.get("/",async(req,res,next)=>{
+  console.log(req.body);
 try {
   const response=await Band.find(req.query)
   res.status(200).json(response)
+  console.log(response);
 } catch (error) {
   next (error)
   
@@ -31,18 +34,20 @@ try {
 
 })
 
-router.get("/random", async(req, res, next)=>{
-  console.log();//-----Take care of this
+//------------- localhost:5005/api/band/random------ get a random band
+router.get("/random", async (req, res, next) => {
   try {
-    const randomBand= Math.floor(Math.random()*data.length)
-const randomb=data[randomBand]
-res.status(200).json(randomb)
+    const response = await Band.find();
+     const randomIndex = Math.floor(Math.random() * response.length);
+    const randomBand = response[randomIndex];
+    res.json(randomBand);
+    
+   
   } catch (error) {
-    next(error)
+    next(error);
   }
+});
 
-
-}) //------------- need to check this route, to get a random band--
 
 //localhost:5005/api/band/:bandId-----------get a band by id-----------------
 router.get("/:bandId",async(req, res, next)=>{
